0dfe69a6498fd9a819ce1f42ac465002eb5cec8d
Justin Pham <justin.tk.pham@gmail.com>
2022-08-17 20:02:31 +1000

    async initial fetch for shop working

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -1,32 +1,42 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Items } from './Items';
+import { Game } from './util/Interfaces';
+
+let init = [
+  {
+    name: 'Grand Theft Auto V',
+    image: '/here',
+    price: 12,
+  },
+  {
+    name: 'Valorant',
+    image: '/there',
+    price: 420,
+  }
+]
 
 export const Shop = () => {
-  let init = [
-    {
-      name: 'Grand Theft Auto V',
-      image: '/here',
-      price: 12,
-    },
-    {
-      name: 'Valorant',
-      image: '/there',
-      price: 420,
-    }
-  ]
   const [items, setItems] = useState(init);
 
-  // const key = '6cc02bd22dd4452bbe0efcabb76d2956';
+  const key = '6cc02bd22dd4452bbe0efcabb76d2956';
 
-  // async function getGames() {
-  //     const response = await fetch(`https://api.rawg.io/api/games?key=${key}&metacritic=80,100&page_size=16`);
-  //     const data = await response.json();
-  //     const games = data['results'];
-  //     return games;
-  // }
-  // getGames().then(games => {
-  //     console.log(games);
-  // })
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`https://api.rawg.io/api/games?key=${key}&metacritic=80,100&page_size=16`);
+      const data = await response.json();
+      const gameList = data['results'].map((game: any) => {
+        return {
+          name: game.name,
+          image: game['background-image'],
+          price: 12,
+        }
+      });
+      console.log('inside fetchGames()');
+      setItems(gameList);
+    }
+
+    fetchData().catch(console.error);
+  }, [])
 
   return (
     <div>
